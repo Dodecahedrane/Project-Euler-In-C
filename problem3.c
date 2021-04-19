967198a4c98024083b533675b7b1a4e877c96963
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-04-19 17:24:29 +0100

    intial commit

---
+++ problem3.c
@@ -0,0 +1,12 @@
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//What is the largest prime factor of the number 600851475143 ?
+
+#include<stdio.h>
+#include<math.h>
+
+#define n 600851475143;
+
+void main(){
+  printf("Project Euler - Problem 3\n\n");
+
+}
