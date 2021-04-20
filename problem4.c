761c50a24c08c02001ffa174ad4c6c23a131a225
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-04-20 10:17:22 +0100

    initial commit

---
+++ problem4.c
@@ -0,0 +1,16 @@
+//A palindromic number reads the same both ways. The largest palindrome made
+//from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+//Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+//Is there a way to do this without just brute forcing it??????
+
+#include<stdio.h>
+#include<math.h>
+
+
+void main(){
+  printf("Project Euler - Problem 4\n\n");
+
+}
