650f1a71f8be61b14733ce68729a04a203e15b35
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-10-22 11:25:19 +0100

    Problem 7 completed

--- problem7.c
+++ problem7.c
@@ -1,3 +1,39 @@
 //By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
 //What is the 10 001st prime number?
+
+#include<stdio.h>
+#include<math.h>
+#include <stdbool.h>
+
+bool isPrime(int n){
+    //https://en.wikipedia.org/wiki/Primality_test
+    for(int i = 2; i < n; i++){
+        if(n%i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+void main(){
+  printf("Project Euler - Problem 7\n\n");
+
+  int primesFound = 0;
+  int maxPrime = 0;
+  int i = 1;
+  while(primesFound != 10001){
+      if(isPrime(i)){
+          printf("Primes Found: %i\n", primesFound);
+
+          primesFound++;
+          maxPrime = i;
+          i = i + 2;  //+2 because no primes are even
+      } else{
+        i = i + 2;  //+2 because no primes are even
+      }
+  }
+
+
+  printf("10,001th prime is %i \n", maxPrime);
+}
