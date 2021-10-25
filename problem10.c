1cfff31d98ea0d38b854324244d2a0bfcb7be429
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-10-25 20:38:11 +0100

    Problem 10 Initial Commit

---
+++ problem10.c
@@ -0,0 +1,12 @@
+//The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+//Find the sum of all the primes below two million.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<stdbool.h>
+
+void main(){
+  printf("Project Euler - Problem 10\n\n");
+}
